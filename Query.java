747bc3c2fa7e54ce3eda97a25d47a552877adb25
sopan khosla <khoslasopan@gmail.com>
2014-10-21 14:28:08 +0530

    created a basic structure of the app

---
+++ Query.java
@@ -0,0 +1,25 @@
+public class Query{
+    private String dName;
+    private String tName;
+    public Query(String d,String t){
+        dName=d;
+        tName=t;
+        /*
+            go to folder dName
+            go to file tName
+        */
+    }
+    public Insert insert(JSON object){
+        /**/
+    }
+    public Find find(String f,JSON object){
+        /**/
+    }
+    public Delete delete(String d,JSON object){
+        /**/
+    }
+    public Update update(String u,JSON object1,JSON object2){
+        /**/
+    }
+
+}
---
+++ Table.java
@@ -0,0 +1,17 @@
+public class Table{
+    private String name;
+    private int numDocs;
+
+    public Table(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getNumDocs(){
+        return numDocs;
+    }
+    //create doc need not be created as it is already presented in Query
+
+
+}
