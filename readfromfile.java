941af9ccdf0c80e6e43bb4de1f8a9c4219a7dbdf
sopan khosla <khoslasopan@gmail.com>
2014-10-25 17:39:41 +0530

    used gson library to convert json object to Hashmap

--- readfromfile.java
+++ readfromfile.java
@@ -1,59 +1,51 @@
-import java.io.FileNotFoundException;  
-import java.io.FileReader;  
-import java.io.IOException;  
-import java.util.Iterator;  
-import org.json.simple.JSONArray;  
-import org.json.simple.JSONObject;  
-import org.json.simple.parser.JSONParser;  
-import org.json.simple.parser.ParseException; 
+  import java.io.FileNotFoundException;  
+  import java.io.FileReader;  
+  import java.io.IOException;  
+  import java.util.Iterator;  
+  import org.json.simple.JSONArray;  
+  import org.json.simple.JSONObject;  
+  import org.json.simple.parser.JSONParser;  
+  import org.json.simple.parser.ParseException; 
 
-import com.google.gson.Gson; 
+  import com.google.gson.Gson; 
 
-public void readFromFile() {  
-  
-  JSONParser parser = new JSONParser();  
-  
-  try {  
-  
-    Object obj = parser.parse(new FileReader(tname));// change to exact location of the file just written a prototype  
+  public Map readFromFile(){  
+    
+    JSONParser parser = new JSONParser();  
+    
+    try {  
+    
+      Object obj = parser.parse(new FileReader(tname));// change to exact location of the file just written a prototype  
 
-    JSONObject jsonObject = (JSONObject) obj; 
-    String data = jsonObject.get(tname).toString(); 
+      JSONObject jsonObject = (JSONObject) obj; 
+      String data = jsonObject.get(tname).toString(); 
 
-    // the data is stored like this 
-    /*
+      Gson gson = new Gson();
+      Map <String,Object> mapData = new HashMap<String,Object>();
+      mapData = (Map <String,Object> ) gson.fromJson(data,map.getClass());
+      
+      return mapData;
 
-      tname:{
-        [
-          {record1},{record2}, etc.........
-        ]
-      }
+      // the data is stored like this 
+      /*
 
-    /*String nameOfCountry = (String) jsonObject.get("Name");  
-    System.out.println("Name Of Country: "+nameOfCountry);  
-
-    long population = (long) jsonObject.get("Population");  
-    System.out.println("Population: "+population);  
-
-    System.out.println("States are :");  
-    JSONArray listOfStates = (JSONArray) jsonObject.get("States");  
-    Iterator<String> iterator = listOfStates.iterator();  
-    while (iterator.hasNext()) {  
-      System.out.println(iterator.next());  
+        tname:
+          {
+              [
+                  {record1},{record2}, etc.........
+              ]
+          }
+      */
+      
+    } 
+    catch (FileNotFoundException e) {  
+     e.printStackTrace();  
+    } 
+    catch (IOException e) {  
+     e.printStackTrace();  
+    } 
+    catch (ParseException e) {  
+     e.printStackTrace();  
     }  
-    */
-
-
-
-  } 
-  catch (FileNotFoundException e) {  
-   e.printStackTrace();  
-  } 
-  catch (IOException e) {  
-   e.printStackTrace();  
-  } 
-  catch (ParseException e) {  
-   e.printStackTrace();  
-  }  
-  
-}
+    
+  }
