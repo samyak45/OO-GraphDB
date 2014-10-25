30a8fadbf5eab1a6c18d90cd547398a841a88e3e
samyak45 <samyak.choudhary@gmail.com>
2014-10-25 12:41:56 +0530

    Added deleteSpecific & deleteConditional methods
    The deleteSpecific method doesn't seem to require any modifications however the deleteConditional method has been implemented in brute force and has time complexity O(m^2) and can be optimized to O(m*logm) or O(m*n).
    Here, m= no. of columns in table and n= no, of rows in table.

--- delete.java
+++ delete.java
@@ -1,7 +1,43 @@
 public class Delete extends Query{
-    private JSON data;
+    private JSONObject input;
+    private JSONAray data;
     public Delete(String dl,JSON object){
-        data=object;
+        input=object;
+        //load data from file
     };
     //helper functions
+    
+    public void deleteSpecific(){
+        int position=getPosition(input);
+        if(position==-1) return;
+        JSONArray list=new JSONArray();
+        if (data != null) { 
+            for (int i=0;i<data.length();i++){ 
+            //Excluding the item at position
+                if (i != position) list.put(data.get(i));
+            }
+        }
+        data=list;
+    }
+    public void deleteConditional(String conditions){
+        //brute force implementation follows, add optimized implementation latter
+        Find find=new Find(conditions,input);
+        int[] indices=Find.satisfies();
+        //sort(indices);
+        for(int i=0;i<indices.length();i++)
+            deleteSpecific(data.get(indices[i]);
+        return;
+    }
+    private int getPosition(JSONObject object){
+        //int pos=-1;
+        if(object==null) return -1;
+        String valA,valB;
+        for(int i=0;i<data.length();i++) 
+            for each_key in object {
+                valA=object.get(each_key);
+                valB=data.get(i).get(each_key);
+                if(valA==valB) return i;
+            }
+                
+    }
 }
