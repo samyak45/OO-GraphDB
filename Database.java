134cf0500cec87ee68870cde94d60f7c149785a8
sidak <spssachdeva21695@gmail.com>
2014-11-01 11:46:50 +0530

    add libraries for json and fix minor bugs

---
+++ Database.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Database {
+	private String name;
+	//private HashMap tables;
+	private int numTables;
+
+	public Database(String name) {
+		this.name = name;
+		// tables = new HashMap();
+		// create folder having the name name
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getNumTables() {
+		return numTables;
+	}
+
+	public Table createTable(String tName) {
+		// create a new file with the name tName in folder name
+		// TODO : insert the name of the table to metadata
+		Table table = new Table(tName);
+		return table;
+	}
+
+	public Table getTable(String tName) {
+		// returns a table with name tName from metadata
+		// TODO : access to metadata
+		//return tables(tName);
+		return null;
+	}
+
+}
