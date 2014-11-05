f758d4ed12da50b9474e2aa05616b1a4f5b3a230
sidak <spssachdeva21695@gmail.com>
2014-11-06 01:07:31 +0530

    Added method for initialising Table with a new json file

--- Table.java
+++ Table.java
@@ -1,17 +1,32 @@
-public class Table{
-    private String name;
-    private int numDocs;
+import java.io.File;
+import java.io.IOException;
 
-    public Table(String name){
-        this.name=name;
-    }
-    public String getName(){
-        return name;
-    }
-    public int getNumDocs(){
-        return numDocs;
-    }
-    //create doc need not be created as it is already presented in Query
+public class Table {
+	private String name;
+	private int numDocs;
 
+	public Table(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getNumDocs() {
+		return numDocs;
+	}
+
+	// create doc need not be created as it is already presented in Query
+	private void initTable() {
+		File file = new File(Database.baseDir + name + ".json");
+		boolean blnCreated = false;
+		try {
+			blnCreated = file.createNewFile();
+		} catch (IOException ioe) {
+			System.out.println("Error while creating a new empty file :" + ioe);
+		}
+	
+	}
 
 }
